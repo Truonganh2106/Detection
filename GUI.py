3d950a4b9576eef0a67f89ad7e745830b130597d
Truonganh2106 <truonganh210601@gmail.com>
2023-10-05 22:12:23 +0700

    update gui

--- GUI.py
+++ GUI.py
@@ -3,7 +3,6 @@ from tkinter import filedialog
 from PIL import Image, ImageTk
 from keras.models import load_model
 import numpy as np
-import os
 
 class MaskPredictionApp:
     def __init__(self, root):
